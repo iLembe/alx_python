d38e2349ea258392cf4a9a9521319bd8bcee1d2e
iLembe <mondli99zulu@gmail.com>
2023-09-18 00:18:17 +0200

    Fixed errors

--- python-more_data_structures/2-update_dictionary.py
+++ python-more_data_structures/2-update_dictionary.py
@@ -1,2 +1,10 @@
 def update_dictionary(a_dictionary, key, value):
-    a_dictionary[key] = value
+    if key in a_dictionary:
+        a_dictionary[key].append(value)
+    else:
+        a_dictionary[key] = [value]
+
+for k, v in a_dictionary.items():
+    print(f"{k}: {v}")
+
+print("xx") 
