f8825e8bed073308434e6c0c445ac1b2530b4c0c
iLembe <mondli99zulu@gmail.com>
2023-09-18 00:38:20 +0200

    Fixed errors

--- python-more_data_structures/2-update_dictionary.py
+++ python-more_data_structures/2-update_dictionary.py
@@ -1,19 +1,4 @@
+#!/usr/bin/env python3
 def update_dictionary(a_dictionary, key, value):
-    if key in a_dictionary:
-        a_dictionary[key] = value
-    else:
-        a_dictionary[key] = value
+    a_dictionary[key] = value
 
-def print_sorted_dictionary(my_dict):
-    sorted_keys = sorted(my_dict.keys())
-    for key in sorted_keys:
-        print("{}: {}".format(key, my_dict[key]))
-
-a_dictionary = {}
-update_dictionary(a_dictionary, 'a', 'a')
-
-print_sorted_dictionary(a_dictionary)
-print("xx")
-
-update_dictionary(a_dictionary, 'a', 'a')
-print_sorted_dictionary(a_dictionary)
