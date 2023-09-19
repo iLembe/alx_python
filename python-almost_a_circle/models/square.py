f38e36db1e785c89da00e0ab41cd334d8e28659e
iLembe <mondli99zulu@gmail.com>
2023-09-20 00:11:44 +0200

    Updating the Square Class by adding the SIZE

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -17,6 +17,21 @@ class Square(Rectangle):
         """
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """Getter method for size."""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Setter method for size.
+
+        Args:
+            value (int): The size value to set.
+        """
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """Return a string representation of the Square instance."""
         return "[Square] ({}) {}/{} - {}".format(
