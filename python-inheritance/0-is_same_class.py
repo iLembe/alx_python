466b328f26160cdcbcbfaa01f2ddc9a9adf24886
iLembe <mondli99zulu@gmail.com>
2023-09-23 21:58:27 +0200

    Check

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -1,6 +1,14 @@
+"""
+A docstring is needed here
+
+"""
 #!/usr/bin/python3
 def is_same_class(obj, a_class):
-    return type(obj) is a_class   
+    return type(obj) is a_class
+
+""""
+A DocString
+"""   
 
 a = ""
 result = is_same_class(a, int)
