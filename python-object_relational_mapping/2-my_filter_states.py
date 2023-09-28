c5522752b6e5c90f756fdd31b0aca4e37dcb65da
iLembe <mondli99zulu@gmail.com>
2023-09-28 15:25:06 +0200

    My Filtered States

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # Check for the correct number of command-line arguments
+    if len(sys.argv) != 5:
+        print("Usage: {} <username> <password> <database> <state_name>".format(sys.argv[0]))
+        sys.exit(1)
+
+    # Command-line arguments
+    username, password, database, state_name = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
+
+    try:
+        # Connect to the MySQL server
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database
+        )
+        cursor = db.cursor()
+
+        # Execute the SQL query to select states where name matches the provided argument
+        cursor.execute(
+            "SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC",
+            (state_name,)
+        )
+
+        # Fetch all the rows
+        rows = cursor.fetchall()
+
+        # Display the results
+        for row in rows:
+            print(row)
+
+    except MySQLdb.Error as e:
+        print("MySQL Error: {}".format(e))
+        sys.exit(1)
+
+    finally:
+        # Close the database connection
+        if db:
+            db.close()
