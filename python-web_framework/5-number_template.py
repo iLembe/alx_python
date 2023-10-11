f462b62054a51aef10e2259a366cd05b1432d104
iLembe <mondli99zulu@gmail.com>
2023-10-11 09:11:28 +0200

    Flask WebApp

--- python-web_framework/5-number_template.py
+++ python-web_framework/5-number_template.py
@@ -16,7 +16,7 @@ The script uses the option strict_slashes=False in route definitions.
 
 from flask import Flask, render_template
 
-app = Flask(__name)
+app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
