5c223a479eeb35654038bab8aad8f84003b5b527
iLembe <mondli99zulu@gmail.com>
2023-10-11 08:26:58 +0200

    MY FIRST FRAMEWORK FILE

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -1,3 +1,7 @@
+"""
+This is a simple Flask web application that displays 'Hello HBNB!' on the root URL.
+"""
+
 from flask import Flask
 
 app = Flask(__name)
@@ -5,6 +9,7 @@ app = Flask(__name)
 # Route definition with strict_slashes=False
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """Display 'Hello HBNB!' when the root URL is accessed."""
     return "Hello HBNB!"
 
 if __name__ == "__main__": # type: ignore
