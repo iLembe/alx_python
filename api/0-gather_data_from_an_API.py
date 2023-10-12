515b6bfbd000c58dc8c9084e477b37701267baa3
iLembe <mondli99zulu@gmail.com>
2023-10-12 21:55:08 +0200

    Check API

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -2,14 +2,17 @@ import requests
 import sys
 
 def gather_employee_data(employee_id):
-    # Endpoint URLs
     employee_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
     todos_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
 
-    # Make requests to get employee details and TODO list
     try:
+        # Fetch data
         employee_response = requests.get(employee_url)
         todos_response = requests.get(todos_url)
+
+        # Check for errors
+        employee_response.raise_for_status()
+        todos_response.raise_for_status()
 
         employee_data = employee_response.json()
         todos_data = todos_response.json()
@@ -18,14 +21,12 @@ def gather_employee_data(employee_id):
             print("Employee not found.")
             return
 
-        employee_name = employee_data.get('name')
-        completed_tasks = [task for task in todos_data if task['completed']]
+        employee_name = employee_data["name"]
+        completed_tasks = [task for task in todos_data if task["completed"]]
         total_tasks = len(todos_data)
 
-        # Print employee's TODO list progress
         print(f"Employee {employee_name} is done with tasks ({len(completed_tasks)}/{total_tasks}):")
-        
-        # Print the titles of completed tasks
+
         for task in completed_tasks:
             print(f"\t{task['title']}")
 
@@ -36,5 +37,6 @@ if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: python 0-gather_data_from_an_API.py <employee_id>")
     else:
-        employee_id = int(sys.argv[1])
+        employee_id = sys.argv[1]
         gather_employee_data(employee_id)
+
